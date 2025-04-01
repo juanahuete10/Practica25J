b3b83e624ca62a8980257837f47f0b5331b15e49
juana10 <juanahuete18@gmail.com>
2025-03-31 19:16:04 -0600

    Correciones

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,8 @@ import Encabezado from "./components/Encabezado";
 import Inicio from "./views/Inicio";
 import Categorias from "./views/categorias";// importacion de categorias
 import Productos from "./views/Productos";
-import Catalogo from "./views/Catalogo";
 import Libros from "./views/Libros";
-
+import Catalogo from "./views/catalogo";
 
 import './App.css'
 
